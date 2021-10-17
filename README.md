ba839568a436619aeaa5a8de031accfcd9bbae85
Alexandre França de Magalhães <alexandredemagalhaess@gmail.com>
2021-10-17 12:45:50 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# DataEngineeringChallenge
-Data Engineering Challenge for Jobsity
+# Trips by week ETL - Jobsity Data Engineering Challenge
+This software purpose is to extract information from predefined csv files containing data from various trips across some regions. It delivers a week average of trips by a specified criteria of the user. It also uses a scalable python rest api to process data and deliver results into a SQLite database. This software is able to scale out with a simple parameter on the docker compose command line.
+
+## Prerequisites
+- Windows 10/11 with WSDL (hasn't been tested on Linux distros)
+- Docker
+- Docker Compose
+- Python
+
+## Run
+Execute run_program.bat or
+```
+docker-compose up -d --scale webetlapp=5
+```
+If you want to change the scale factor, just change the number after webetlapp on the command above.
+
+## Folder Structure
+```
+docker-webetlapp
+│   README.md
+│   app.py   
+│   requirements.txt
+│   dockerfile
+│   docker-compose.yml 
+│   nginx.conf
+│   run_program.bat
+└─── files
+│   │   trips.csv
+│   │   trips_large.csv
+└───database
+    │   trips.db
+```
+
+## Instructions
+
+The csv files to be processed must be placed on the "files" directory. To process files there are two endpoins that can be used. One is to specify a region and the other to specify a box of coordinates to filter the results. This docker app uses a load balancer which listens requests on localhost:4000 host and port.
+
+The available endpoints are
+```
+http://localhost:4000/coordinates/<origindest>/<upper_v_lat>/<lower_v_lat>/<upper_v_lon>/<lower_v_lon>/<file_name>
+http://localhost:4000/region/<region>/<file_name>
+```
+For the coordinates one, you must specify the parameters as such:
+- <origindest> = "origin" or "dest". Lower and upper limits applied to origin or destination lat and lon coordinates
+- <upper_v_lat> = Upper limit for latitude EX: 14.858954546
+- <lowerr_v_lat> = Lower limit for latitude EX: 5.8987646845
+- <upper_v_lon> = Upper limit for longitude EX: 14.858954546
+- <lower_v_lon> = Lower limit for longitude EX: 5.8987646845
+- <file_name> = Exact file name on the "files folder" without .csv suffix. EX: trips.csv -> trips
+
+For region, you must specify the parameters as such:
+- <region> = Exact name of the region. Ex: Prague
+- <file_name> = Exact file name on the "files folder" without .csv suffix. EX: trips.csv -> trips
+
+Sample Requests:
+```
+http://localhost:4000/region/Prague/trips
+http://localhost:4000/coordinates/origin/55.98756/4.5487457/60.6547854/5.654784589/trips
+```
+
+## Output
+
+To check the output, connect to the SQLite database. The tables are:
+```
+weekly_trips_by_coordinates
+weekly_trips_by_region
+```
+
+The software currently is idempotent. Meaning that for a given input you will have the same output every time. The purpose of that is that you can check results for a given input without the interference of previous ones. To check the data inside database i recommend https://sqlitebrowser.org/ as a first choice for it's simplicity. Just open the file "trips.db" inside the database folder to check results.
+
+## License
+Copyright (c) 2021 Alexandre França de Magalhães
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ app.py
@@ -0,0 +1,133 @@
+import datetime
+import pandas as pd
+import flask
+from sqlalchemy import create_engine
+
+def extract(file_name):
+    df = pd.read_csv(f"./files/{file_name}.csv")
+    return df
+
+def transform_region(df, region_filter):
+    #getting coordinates on indivitual lat and lon columns
+    df['origin_coord_lat'] = df['origin_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['origin_coord_lat'] = df['origin_coord_lat'].str.split(' ')
+    df['origin_coord_lat'] = pd.to_numeric(df['origin_coord_lat'].str[0])
+
+    df['origin_coord_lon'] = df['origin_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['origin_coord_lon'] = df['origin_coord_lon'].str.split(' ')
+    df['origin_coord_lon'] = pd.to_numeric(df['origin_coord_lon'].str[1])
+
+    df['destination_coord_lat'] = df['destination_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['destination_coord_lat'] = df['destination_coord_lat'].str.split(' ')
+    df['destination_coord_lat'] = pd.to_numeric(df['destination_coord_lat'].str[0])
+
+    df['destination_coord_lon'] = df['destination_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['destination_coord_lon'] = df['destination_coord_lon'].str.split(' ')
+    df['destination_coord_lon'] = pd.to_numeric(df['destination_coord_lon'].str[1])
+
+    #converts to datetime
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    #group by similar origin/destination/time
+    df = df.groupby(df.columns.tolist(), as_index=False).size()
+
+    #filter by region
+    df = df[df['region'] == region_filter]
+
+    #groupBy region
+    df['datetime'] = pd.to_datetime(df['datetime']) - pd.to_timedelta(7, unit='d')
+    df = df.groupby(['region', pd.Grouper(key='datetime', freq='W-MON')])['size']\
+    .sum()\
+    .reset_index()\
+    .sort_values('datetime')
+
+    df['size'] = df['size'] / 7
+
+    df.rename(columns={'datetime':'week', 'size':'week_average'}, inplace=True)
+
+    return df
+
+def transform_coordinates(df, origin_dest, upper_value_lat, lower_value_lat, upper_value_lon, lower_value_lon):
+    #getting coordinates on indivitual lat and lon columns
+    df['origin_coord_lat'] = df['origin_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['origin_coord_lat'] = df['origin_coord_lat'].str.split(' ')
+    df['origin_coord_lat'] = pd.to_numeric(df['origin_coord_lat'].str[0])
+
+    df['origin_coord_lon'] = df['origin_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['origin_coord_lon'] = df['origin_coord_lon'].str.split(' ')
+    df['origin_coord_lon'] = pd.to_numeric(df['origin_coord_lon'].str[1])
+
+    df['destination_coord_lat'] = df['destination_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['destination_coord_lat'] = df['destination_coord_lat'].str.split(' ')
+    df['destination_coord_lat'] = pd.to_numeric(df['destination_coord_lat'].str[0])
+
+    df['destination_coord_lon'] = df['destination_coord'].str.extract(r"(?<=POINT \()(.*)(?=\))")
+    df['destination_coord_lon'] = df['destination_coord_lon'].str.split(' ')
+    df['destination_coord_lon'] = pd.to_numeric(df['destination_coord_lon'].str[1])
+
+    #converts to datetime
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    #group by similar origin/destination/time
+    df = df.groupby(df.columns.tolist(), as_index=False).size()
+
+    #replace if values came with comma instead of dot
+    upper_value_lat = float(upper_value_lat.replace(',', '.'))
+    lower_value_lat = float(lower_value_lat.replace(',', '.'))
+    upper_value_lon = float(upper_value_lon.replace(',', '.'))
+    lower_value_lon = float(lower_value_lon.replace(',', '.'))
+
+    #checks whether origin or dest
+    if origin_dest == 'origin':
+        df = df[(df['origin_coord_lat'] >= lower_value_lat)
+                & (df['origin_coord_lat'] <= upper_value_lat)
+                & (df['origin_coord_lon'] <= upper_value_lon)
+                & (df['origin_coord_lon'] >= lower_value_lon)
+        ]
+    if origin_dest == 'dest':
+        df = df[(df['dest_coord_lat'] >= lower_value_lat)
+                & (df['dest_coord_lat'] <= upper_value_lat)
+                & (df['dest_coord_lon'] <= upper_value_lon)
+                & (df['dest_coord_lon'] >= lower_value_lon)
+        ]
+
+    #groupBy region
+    df['datetime'] = pd.to_datetime(df['datetime']) - pd.to_timedelta(7, unit='d')
+    df = df.groupby(['region', pd.Grouper(key='datetime', freq='W-MON')])['size']\
+    .count()\
+    .reset_index()\
+    .sort_values('datetime')
+
+    df['size'] = df['size']/7
+
+    df.rename(columns={'datetime':'week', 'size':'week_average'}, inplace=True)
+
+    return df
+
+def load(df, table_name):
+    engine = create_engine('sqlite:////database/trips.db', echo=True)
+    sqlite_connection = engine.connect()
+    df.to_sql(table_name, sqlite_connection, if_exists='replace') #change here to 'append' if you want to load multiple different files.
+    sqlite_connection.close()
+    return True
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/coordinates/<origindest>/<upper_v_lat>/<lower_v_lat>/<upper_v_lon>/<lower_v_lon>/<file_name>', methods=['GET'])
+def tolerance_func(origindest, upper_v_lat, lower_v_lat, upper_v_lon, lower_v_lon,file_name):
+    table_name = "weekly_trips_by_coordinates"
+    df = extract(file_name)
+    df = transform_coordinates(df, origindest, upper_v_lat, lower_v_lat, upper_v_lon, lower_v_lon)
+    load(df, table_name)
+    return f"<h1>Load Trips Data</h1><p>Load has completed with defined parameters for {origindest}</p>"
+
+@app.route('/region/<region>/<file_name>', methods=['GET'])
+def region_func(region,file_name):
+    table_name = "weekly_trips_by_region"
+    execution_date = datetime.datetime.now()
+    df = extract(file_name)
+    df = transform_region(df, region)
+    load(df, table_name)
+    return f"<h1>Load Trips Data</h1><p>Load has completed with defined parameters for {region}</p>"
+
